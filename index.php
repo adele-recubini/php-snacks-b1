996e279b6cf27219d2ee67f4ea700589e20a53fe
adele <adele.r87@gmail.com>
2021-02-26 15:59:22 +0100

    snack2

--- index.php
+++ index.php
@@ -31,6 +31,32 @@ for ($i=0; $i < count($matches); $i++) {
   echo $matches[$i]["casa"] . '-' .  $matches[$i]["ospite"] . $matches[$i]["puntiCasa"] . '|'. $matches[$i]["puntiOspite"];
 }
 
-
-
  ?>
+
+ <br><br><br>
+
+<!--
+ Passare come parametri GET name, mail e age e verificare (cercando i metodi che non
+ conosciamo nella documentazione) che:
+ 1. name sia più lungo di 3 caratteri,
+ 2. mail contenga un punto e una chiocciola
+ 3. age sia un numero.
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+ <?php
+
+ $name = $_GET["name"];
+ $mail = $_GET["mail"];
+ $age = $_GET["age"];
+
+
+ if (strpos($mail, '.') && strpos($mail, '@') && strlen($name) > 3  && is_numeric($age))  {
+   echo "Accesso consentito";
+ }
+ else {
+   echo "accesso negato";
+ }
+
+
+
+  ?>
